7fbe7d155dae1e285835c48f9e409691024c2e73
Ksenia Koldaeva <kseniacold@gmail.com>
2017-09-23 12:07:28 -0700

    Test git username

--- src/main/java/com/cs56fitnessapp/models/Day.java
+++ src/main/java/com/cs56fitnessapp/models/Day.java
@@ -9,5 +9,6 @@ package com.cs56fitnessapp.models;
 public class Day {
     public static void main(String [] args) {
         System.out.println("Hello");
+        System.out.println("Test message");
     }
 }
