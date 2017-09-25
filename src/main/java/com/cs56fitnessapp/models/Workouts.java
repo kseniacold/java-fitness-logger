bcc76658f4858af4f17030ef6779cfb39189a2f8
wowitsdanny <dancervantes17@gmail.com>
2017-09-24 18:30:38 -0700

    Workouts class

--- src/main/java/com/cs56fitnessapp/models/Workouts.java
+++ src/main/java/com/cs56fitnessapp/models/Workouts.java
@@ -1,10 +1,12 @@
-/*Daniel Cervantes
+/*
+* @author Daniel Cervantes
 * Date created: 9/24/17
 * Last updated: 9/24/17
-* */
+ */
 
 
 package com.cs56fitnessapp.models;
 
 public class Workouts {
+
 }
