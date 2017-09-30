561da21cc58594f99cb40674a5e691244afce10a
wowitsdanny <dancervantes17@gmail.com>
2017-09-29 23:10:49 -0700

    Added ArrayList with workouts

--- src/main/java/com/cs56fitnessapp/models/Workout.java
+++ src/main/java/com/cs56fitnessapp/models/Workout.java
@@ -7,6 +7,26 @@
 
 package com.cs56fitnessapp.models;
 
+import java.util.ArrayList;
+
 public abstract class Workout implements BurningCalories {
 
+    ArrayList<String> exerciseList = new ArrayList<String>(){{
+
+        add("\nBench Press\n");
+        add("Bicep Curls\n");
+        add("Shoulder Press\n");
+        add("Lateral Raises\n");
+        add("Tricep Extensions\n");
+        add("Bent Over Rows\n");
+        add("Sit Ups\n");
+        add("Back Squats\n");
+        add("Front Squats\n");
+        add("Deadlift\n");
+        add("Leg Press\n");
+        add("Lunges\n");
+        add("Kettle Bell Swings\n");
+        add("Leg Lifts\n");
+
+    }};
 }
