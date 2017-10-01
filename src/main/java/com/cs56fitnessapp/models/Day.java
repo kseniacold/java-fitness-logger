e999be70f640ab8d9b66ead2e09c7df076f86fad
Ksenia Koldaeva <kseniacold@gmail.com>
2017-10-01 12:36:37 -0700

    Refactor: BurningCalories -> ActivityFacts, ConsumingCalories -> DietFacts. Implemented ActivityFacts, DietFacts. Added override interface methods signatures in the classes that use them.

--- src/main/java/com/cs56fitnessapp/models/Day.java
+++ src/main/java/com/cs56fitnessapp/models/Day.java
@@ -3,10 +3,44 @@ package com.cs56fitnessapp.models;
 /**
  * @author Ksenia Koldaeva
  * Created: 9/17/17
- * Last Updated: 9/17/17
+ * Last Updated: 10/1/17
  */
 
-public class Day {
+public class Day implements ActivityFacts, DietFacts {
+
+
+    /* ActivityFacts Interface */
+    @Override
+    public int getCaloriesOut() {
+        return 0;
+    }
+
+    @Override
+    public int getActiveTime() {
+        return 0;
+    }
+
+    /* DietFacts Interface */
+    @Override
+    public int getCaloriesIn() {
+        return 0;
+    }
+
+    @Override
+    public TotalFat getTotalFatIn() {
+        return null;
+    }
+
+    @Override
+    public TotalCarb getTotalCarbIn() {
+        return null;
+    }
+
+    @Override
+    public double getProteinIn() {
+        return 0;
+    }
+
     public static void main(String [] args) {
         System.out.println("Hello");
     }
--- src/main/java/com/cs56fitnessapp/models/Workout.java
+++ src/main/java/com/cs56fitnessapp/models/Workout.java
@@ -1,12 +1,19 @@
-/*
-* @author Daniel Cervantes
-* Date created: 9/24/17
-* Last updated: 9/24/17
- */
-
-
 package com.cs56fitnessapp.models;
 
-public abstract class Workout implements BurningCalories {
+/**
+ * @author Daniel Cervantes
+ * Date created: 9/24/17
+ * Last updated: 9/24/17
+ */
 
+public abstract class Workout implements ActivityFacts {
+    @Override
+    public int getCaloriesOut() {
+        return 0;
+    }
+
+    @Override
+    public int getActiveTime() {
+        return 0;
+    }
 }
