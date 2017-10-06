a1ab2fe742adc85cd2696ebcf113173a410da946
Ksenia Koldaeva <kseniacold@gmail.com>
2017-10-06 07:52:38 -0700

    Create: enums Gender, Goal, ActivityLevel

---
+++ src/main/java/com/cs56fitnessapp/models/Gender.java
@@ -0,0 +1,26 @@
+package com.cs56fitnessapp.models;
+
+/**
+ * @author Ksenia Koldaeva
+ * Created: 10/6/17
+ * Last Updated: 10/6/17
+ */
+
+public enum Gender {
+    // FEMALE / MALE options
+    FEMALE("Female"), MALE("Male");
+
+    // String representation
+    private String stringValue;
+
+    Gender(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    /**
+     * @return String representation of Gender Enum
+     */
+    public String getStringValue() {
+        return stringValue;
+    }
+}
---
+++ src/main/java/com/cs56fitnessapp/models/Goal.java
@@ -0,0 +1,26 @@
+package com.cs56fitnessapp.models;
+
+/**
+ * @author Ksenia Koldaeva
+ * Created: 10/6/17
+ * Last Updated: 10/6/17
+ */
+
+public enum Goal {
+    // LOSE, MAINTAIN, GAIN
+    LOSE("Lose weight"), MAINTAIN("Maintain weight"), GAIN("Gain weight");
+
+    // String representation
+    private String description;
+
+    Goal(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return String representation of Gender Enum
+     */
+    public String getDescription() {
+        return description;
+    }
+}
