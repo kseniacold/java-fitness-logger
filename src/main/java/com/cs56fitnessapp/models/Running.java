3670acb061557d285b4693e2f24bcbf925c7e4b0
Ksenia Koldaeva <kseniacold@gmail.com>
2017-10-06 09:56:32 -0700

    Create: Running class

---
+++ src/main/java/com/cs56fitnessapp/models/Running.java
@@ -0,0 +1,29 @@
+package com.cs56fitnessapp.models;
+
+/**
+ * @author Ksenia Koldaeva
+ * Created: 10/6/17
+ * Last Updated: 10/6/17
+ */
+
+public class Running extends Endurance implements ActivityFacts {
+
+    /**
+     * Constructs new Running object with provided distance and duration
+     * @param distance
+     * @param duration
+     */
+    public Running(double distance, double duration) {
+        super(distance, duration);
+    }
+
+    @Override
+    public int getCaloriesOut() {
+        return 0;
+    }
+
+    @Override
+    public int getActiveTime() {
+        return 0;
+    }
+}
