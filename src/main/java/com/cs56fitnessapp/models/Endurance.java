24230a3bfdf395170f27bc27c63fbdb1cd892211
Ksenia Koldaeva <kseniacold@gmail.com>
2017-10-06 09:44:35 -0700

    Create and implement: abstract Endurance class

---
+++ src/main/java/com/cs56fitnessapp/models/Endurance.java
@@ -0,0 +1,26 @@
+package com.cs56fitnessapp.models;
+
+/**
+ * @author Ksenia Koldaeva
+ * Created: 10/6/17
+ * Last Updated: 10/6/17
+ */
+
+public abstract class Endurance extends Workout {
+
+    // distance of an endurance workout
+    private double distance;
+
+    // duration of an endurance workout
+    private double duration;
+
+    /**
+     * Constructs an Endurance object with distance and duration
+     * @param distance distance for an endurance workout
+     * @param duration duration for an endurance workout
+     */
+    public Endurance(double distance, double duration) {
+        this.distance = distance;
+        this.duration = duration;
+    }
+}
