169e220c1acedf7dde5b60d243a1c3d277595b19
Ksenia Koldaeva <kseniacold@gmail.com>
2017-10-10 22:04:23 -0700

    Workout class. Modify: add constructor and getters and setters.[C

--- src/main/java/com/cs56fitnessapp/models/Workout.java
+++ src/main/java/com/cs56fitnessapp/models/Workout.java
@@ -2,28 +2,50 @@ package com.cs56fitnessapp.models;
 
 /**
  * @author Daniel Cervantes
+ * @author Ksenia Koldaeva
  * Date created: 9/24/17
- * Last updated: 10/6/17
+ * Last updated: 10/10/17
  */
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public abstract class Workout implements ActivityFacts {
 
     private User user;
-    private Date localDate;
+    private LocalDate date;
     private int warmUpTime;
     private int coolDownTime;
 
-    @Override
-    public int getCaloriesOut() {
-        return 0;
+    public Workout(User user, LocalDate date) {
+        this.user = user;
+        this.date = date;
     }
 
-    @Override
-    public int getActiveTime() {
-        return 0;
+    // Start getters and setters
+
+    public LocalDate getDate() {
+        return date;
     }
 
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 
+    public int getWarmUpTime() {
+        return warmUpTime;
+    }
+
+    public void setWarmUpTime(int warmUpTime) {
+        this.warmUpTime = warmUpTime;
+    }
+
+    public int getCoolDownTime() {
+        return coolDownTime;
+    }
+
+    public void setCoolDownTime(int coolDownTime) {
+        this.coolDownTime = coolDownTime;
+    }
+
+    // End getters and setters
 }
