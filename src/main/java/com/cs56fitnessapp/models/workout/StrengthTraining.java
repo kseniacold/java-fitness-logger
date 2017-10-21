03e9e62c33e2e77afabb215d8ca2cb2501bab813
wowitsdanny <dancervantes17@gmail.com>
2017-10-21 09:50:56 -0700

    StrengthTraining class with ArrayList of StrengthExercise. Implementation of this class based on UML.

---
+++ src/main/java/com/cs56fitnessapp/models/workout/StrengthTraining.java
@@ -0,0 +1,21 @@
+package com.cs56fitnessapp.models.workout;
+
+import java.util.ArrayList;
+
+/**
+ *  @author Daniel Cervantes
+ *  @author Ksenia Koldaeva
+ *
+ * Date created: 10/18/17
+ * Last updated: 10/21/17
+ */
+
+
+public abstract class StrengthTraining extends Workout {
+
+    private ArrayList<StrengthExercise> exerciseList = new ArrayList<StrengthExercise>(){{
+
+    }};
+
+
+}
