d3649301d7ff0b74089eaa964a0c90acfee4be87
kseniacold <kseniacold@gmail.com>
2017-11-22 13:20:27 -0800

    Gui initial commit. Export from start-gui branch

--- src/main/java/com/cs56fitnessapp/views/FitnessApplication.java
+++ src/main/java/com/cs56fitnessapp/views/FitnessApplication.java
@@ -1,18 +1,32 @@
 package com.cs56fitnessapp.views;
 import com.cs56fitnessapp.SqLiteConnection;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FitnessApplication extends Application{
-     @Override
-    public void start(Stage stage) {
-         try {
-             SqLiteConnection sqLite = new SqLiteConnection();
-             sqLite.getConnection();
-             sqLite.closeConnection();
-         } catch (Exception e) {
-             System.out.println(e.getMessage());
-         }
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        try {
+            SqLiteConnection sqLite = new SqLiteConnection();
+            sqLite.getConnection();
+            sqLite.closeConnection();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        Parent root = FXMLLoader.load(getClass().getResource("welcomeLayout.fxml"));
+        Scene welcomeScene = new Scene(root);
+
+        welcomeScene.getStylesheets().add("https://fonts.googleapis.com/css?family=Heebo:300,400,500,700,800,900|Righteous");
+        welcomeScene.getStylesheets().add(getClass().getResource("../../../resources/application_styles.css").toExternalForm());
+
+        // show welcome scene
+        stage.setScene(welcomeScene);
+        stage.show();
     }
 
     public static void main(String[] args) {
