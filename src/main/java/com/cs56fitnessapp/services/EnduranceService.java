3865eecb37a11327ef57797c54b43dd0d9aba6fe
Ksenia Koldaeva <kseniacold@gmail.com>
2017-12-06 11:34:27 -0800

    Add buttons handlers for changing the scenes

---
+++ src/main/java/com/cs56fitnessapp/services/EnduranceService.java
@@ -0,0 +1,19 @@
+package com.cs56fitnessapp.services;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Ksenia Koldaeva
+ * Created: 12/6/17
+ * Last Updated: 12/6/17
+ */
+
+public class EnduranceService implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/main/java/com/cs56fitnessapp/services/StrengthService.java
@@ -0,0 +1,10 @@
+package com.cs56fitnessapp.services;
+
+/**
+ * @author Ksenia Koldaeva
+ * Created: 12/6/17
+ * Last Updated: 12/6/17
+ */
+
+public class StrengthService {
+}
