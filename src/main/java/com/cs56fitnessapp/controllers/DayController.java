503e380bbf827db36fd6ce6287dcd2f937eeaeea
Ksenia Koldaeva <kseniacold@gmail.com>
2017-12-06 10:22:38 -0800

    Simplify fxml files names. Add event handler for workout button

--- src/main/java/com/cs56fitnessapp/controllers/DayController.java
+++ src/main/java/com/cs56fitnessapp/controllers/DayController.java
@@ -1,10 +1,14 @@
 package com.cs56fitnessapp.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -30,5 +34,24 @@ public class DayController implements Initializable {
         String formattedString = localDate.format(formatter);
 
         title.setText(formattedString);
+    }
+
+    @FXML
+    private void addFood() {
+
+    }
+
+    @FXML void addWorkout() throws IOException {
+        /** Initialize window obj */
+        window = (Stage)title.getScene().getWindow();
+
+
+        /** Redirect to the day scene */
+        root = FXMLLoader.load(getClass().getResource("../views/workout.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("../../../resources/application_styles.css").toExternalForm());
+
+        // show Day scene
+        window.setScene(scene);
     }
 }
